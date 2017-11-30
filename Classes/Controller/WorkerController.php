c3d047d1287963b9266c8b63d3abf03be6eaf7aa
Salvatore Eckel <salvaracer@gmx.de>
2017-11-30 22:58:28 +0100

    Cleanup

--- Classes/Controller/WorkerController.php
+++ Classes/Controller/WorkerController.php
@@ -2,12 +2,12 @@
 
 namespace SalvatoreEckel\T3cms\Controller;
 
-use \TYPO3\CMS\Backend\Backend\Avatar\Avatar;
-use \TYPO3\CMS\Core\Utility\GeneralUtility;
-use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use \TYPO3\CMS\Core\Database\ConnectionPool;
-#use \TYPO3\CMS\Core\Messaging\AbstractMessage;
-use \TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use TYPO3\CMS\Backend\Backend\Avatar\Avatar;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+#use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * WorkerController
@@ -45,7 +45,28 @@ class WorkerController extends AbstractController {
 			}
 		}
 
-		/* Pages Count */
+	# Pages Count
+		$pagesCountAll = $this->countAllPages();
+	# Get loaded t3themes_* extensions keys
+		$loadedThemes = $this->getLoadedExtensionsByPrefix('t3themes');
+		$loadedContents = $this->getLoadedExtensionsByPrefix('t3content');
+
+    	$this->view->assign('pagesCountAll', $pagesCountAll);
+    	$this->view->assign('loadedThemes', $loadedThemes);
+    	$this->view->assign('loadedContents', $loadedContents);
+    	$this->view->assign('beUser', $beUser);
+    	$this->view->assign('beUserCountAll', $beUserCountAll);
+    	$this->view->assign('baseUrl', $baseUrl);
+    	$this->view->assign('currentPage', $currentPage);
+	}
+
+	/**
+	 * Get the count of all pages
+	 *
+	 * @return int
+	 */
+	private function countAllPages()
+	{
 		$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
 		$pagesCountAll = $queryBuilder
 		   ->count('uid')
@@ -54,23 +75,28 @@ class WorkerController extends AbstractController {
 		   ->execute()
 		   ->fetchColumn(0);
 
-	# Get loaded t3themes_* extensions keys
-		$loadedThemes = [];
+		return $pagesCountAll;
+	}
+
+	/**
+	 * Find extensions that begin with "t3themes_" and return an array only of loaded extensions
+	 *
+	 * @param string $prefix
+	 * @return array
+	 */
+	private function getLoadedExtensionsByPrefix($prefix)
+	{
+		$loadedExtensions = [];
 		foreach (scandir(PATH_typo3conf . 'ext/') as $key => $localExtKey) {
-		    if ((preg_match('/t3themes_/i', $localExtKey)) || $localExtKey == 't3themes') {
+		    if ((preg_match('/'.$prefix.'_/i', $localExtKey)) || $localExtKey == $prefix) {
 		    	if (ExtensionManagementUtility::isLoaded($localExtKey)) {
-					$loadedThemes[$localExtKey] = $localExtKey;
+					$loadedExtensions[$localExtKey] = $localExtKey;
 		    	}
 		    }
 		}
-		unset($loadedThemes['t3themes']);
+		unset($loadedExtensions[$prefix]);
 
-    	$this->view->assign('pagesCountAll', $pagesCountAll);
-    	$this->view->assign('loadedThemes', $loadedThemes);
-    	$this->view->assign('beUser', $beUser);
-    	$this->view->assign('beUserCountAll', $beUserCountAll);
-    	$this->view->assign('baseUrl', $baseUrl);
-    	$this->view->assign('currentPage', $currentPage);
+		return $loadedExtensions;
 	}
 
 	/**
@@ -245,47 +271,6 @@ class WorkerController extends AbstractController {
 		}
 
 		return $htmlOptions;
-	}
-
-	/**
-	 * get web page with curl
-	 *
-	 * @param string $url
-	 * @return void
-	 */
-	protected function get_web_page( $url )
-	{
-		$user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
-
-		$options = array(
-
-			CURLOPT_CUSTOMREQUEST 	=>"GET",		//set request type post or get
-			CURLOPT_POST 			=> false,		//set to GET
-			CURLOPT_USERAGENT 		=> $user_agent, //set user agent
-			// CURLOPT_COOKIEFILE 	=>"cookie.txt", //set cookie file
-			// CURLOPT_COOKIEJAR 	=>"cookie.txt", //set cookie jar
-			CURLOPT_RETURNTRANSFER 	=> true, 	// return web page
-			CURLOPT_HEADER 			=> false,	// don't return headers
-			CURLOPT_FOLLOWLOCATION 	=> true, 	// follow redirects
-			CURLOPT_ENCODING 		=> "", 		// handle all encodings
-			CURLOPT_AUTOREFERER 	=> true, 	// set referer on redirect
-			CURLOPT_CONNECTTIMEOUT 	=> 120,  	// timeout on connect
-			CURLOPT_TIMEOUT 		=> 120,  	// timeout on response
-			CURLOPT_MAXREDIRS 		=> 10,   	// stop after 10 redirects
-		);
-
-		$ch  		= curl_init( $url );
-		curl_setopt_array( $ch, $options );
-		$content 	= curl_exec( $ch );
-		$err 		= curl_errno( $ch );
-		$errmsg 	= curl_error( $ch );
-		$header 	= curl_getinfo( $ch );
-		curl_close( $ch );
-
-		$header['errno']   = $err;
-		$header['errmsg']  = $errmsg;
-		$header['content'] = $content;
-		return $header;
 	}
 
 }
